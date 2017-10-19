caf29f01529dd8253bd04f7c7e8c6cb139d677e1
Steven Burns <s.burns@baypath.net>
2017-10-19 09:29:37 -0400

    Added some comments - Steven

--- MartyoAndStaine/BattlePause.cs
+++ MartyoAndStaine/BattlePause.cs
@@ -5,12 +5,12 @@ namespace MartyoAndStaine {
     class BattlePause : GameWindow {
         private SpriteBatch sprite;
 
-        public BattlePause(Game game, SpriteBatch sb) : base(game) { sprite = sb; }
+        public BattlePause(Game game, SpriteBatch sb) : base(game) { sprite = sb; } // Initializes the level with one sprite for the background
 
         public override void Update(GameTime gameTime) { base.Update(gameTime); }
 
         public override void Draw(GameTime gameTime) {
-            sprite.Draw(Game.Content.Load<Texture2D>("battlePause"), new Rectangle(0, 0, 1200, 600), Color.White);
+            sprite.Draw(Game.Content.Load<Texture2D>("battlePause"), new Rectangle(0, 0, 1200, 600), Color.White); // Creates the background
             base.Draw(gameTime);
         }
     }
